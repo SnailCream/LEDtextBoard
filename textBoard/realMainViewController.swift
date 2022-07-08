bc9f2365cf0064863c574169320f968d32e5d5b6
carki <94.nogari@gmail.com>
2022-07-08 17:06:23 +0900

    UICommit

--- textBoard/realMainViewController.swift
+++ textBoard/realMainViewController.swift
@@ -7,23 +7,45 @@
 
 import UIKit
 
-class realMainViewController: UIViewController {
+func btnDesign(btnName: UIButton, btnContent: String) {
+    btnName.setTitle(btnContent, for: .normal)
+    btnName.backgroundColor = .yellow
+    btnName.setTitleColor(.black, for: .normal)
+    
+}
 
+class realMainViewController: UIViewController {
+    @IBOutlet weak var textLabel: UILabel! // 텍스트가 보여지는 레이블
+    
+    @IBOutlet weak var userTextField: UITextField! // 유저가 입력하는 텍스트 필드
+    
+    @IBOutlet weak var btnSend: UIButton! // 전송버튼
+    @IBOutlet weak var btnChangeColor: UIButton! // 색 변경 버튼
+    
+    @IBOutlet var mainView: UIView! // 메인 뷰
+    @IBOutlet weak var embedView: UIView! // 버튼 + 텍스트필드 임베디드 뷰
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainView.backgroundColor = .white
+        embedView.backgroundColor = .lightGray
 
-        // Do any additional setup after loading the view.
+        btnDesign(btnName: btnSend, btnContent: "전송")
+        btnDesign(btnName: btnChangeColor, btnContent: "색 변경")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnSendAction(_ sender: UIButton) {
+        textLabel.text = userTextField.text
+        view.endEditing(true)
+        
     }
-    */
-
+    
+    
+    
+    
+    
+    
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
